5e76bf2de355a55fe7bac9f08365b0fb5b8bc6ba
Alex <fernandesalexandre1996@gmail.com>
2022-11-22 13:44:47 +0100

    feat(proxy-server): add proxy server to nx workspace

---
+++ apps/karmine-proxy-server/src/app/app.js
@@ -0,0 +1,36 @@
+/*!
+ * AllOrigins
+ * written by Gabriel Nunes <gabriel@multiverso.me>
+ * http://github.com/gnuns
+ */
+const express = require('express')
+
+const processRequest = require('./process-request')
+
+function enableCORS(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Content-Encoding, Accept'
+  )
+  res.header(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PATCH, PUT, DELETE'
+  )
+  next()
+}
+
+module.exports = (function app() {
+  const app = express()
+
+  app.set('case sensitive routing', false)
+  app.set('jsonp callback name', 'callback')
+  app.disable('x-powered-by')
+  app.enable("trust proxy")
+  app.use(enableCORS)
+
+  app.all('/:format(get|raw|json|info)', processRequest)
+
+  return app
+})()
